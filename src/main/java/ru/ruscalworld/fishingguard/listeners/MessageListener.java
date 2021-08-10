3baa156439881618a3388187f4caa34e2334d026
RuscalWorld <me@ruscalworld.ru>
2021-08-10 15:46:39 +0300

    Kick members that send suspicious messages

--- src/main/java/ru/ruscalworld/fishingguard/listeners/MessageListener.java
+++ src/main/java/ru/ruscalworld/fishingguard/listeners/MessageListener.java
@@ -1,18 +1,18 @@
 package ru.ruscalworld.fishingguard.listeners;
 
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Invite;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.ruscalworld.fishingguard.FishingGuard;
-import ru.ruscalworld.fishingguard.models.BannedDomain;
+import ru.ruscalworld.fishingguard.models.GuildSettings;
 import ru.ruscalworld.fishingguard.models.Incident;
 import ru.ruscalworld.fishingguard.util.LinkManager;
-import ru.ruscalworld.storagelib.Storage;
 
-import java.net.URL;
-import java.util.List;
 import java.util.Optional;
 
 public class MessageListener extends ListenerAdapter {
@@ -26,6 +26,44 @@ public class MessageListener extends ListenerAdapter {
             if (LinkManager.checkMessage(event.getMessage())) {
                 event.getMessage().delete().queue();
                 Incident.create(event.getMember(), event.getMessage().getContentRaw());
+
+                GuildSettings settings = GuildSettings.getByGuild(event.getGuild());
+                Optional<Invite> invite = settings.getInvite();
+                Member owner = event.getGuild().getOwner();
+                String inviteUrl = event.getJDA().getInviteUrl(Permission.KICK_MEMBERS, Permission.MESSAGE_MANAGE, Permission.CREATE_INSTANT_INVITE);
+
+                StringBuilder message = new StringBuilder();
+                message.append("Привет. Ты получил это сообщение, поскольку мы заметили подозрительную активность на сервере `")
+                        .append(event.getGuild().getName()).append("`. Твой аккаунт был взломан и использован для ")
+                        .append("рассылки фишинговых ссылок. Настоятельно рекомендуем изменить пароль и включить двухфакторную ")
+                        .append("аутентификацию, чтобы подобная ситуация не повторилась. Порекомендуй своим друзьям сделать ")
+                        .append("то же самое, чтобы обезопасить их аккаунты. \n\n");
+
+                if (invite.isPresent()) {
+                    message.append("Если ты был исключён с сервера, то можешь вернуться, используя приглашение: ")
+                            .append(invite.get().getUrl());
+                } else {
+                    message.append("Если ты был исключён с сервера, то тебе следует связаться с его администратором, " +
+                            "т.к. мы не смогли создать приглашение для тебя. ");
+                    if (owner != null) message.append("Владелец сервера `")
+                            .append(event.getGuild().getName()).append("`: ")
+                            .append("`").append(owner.getUser().getAsTag()).append("`.");
+                }
+
+                message.append("\n\n");
+                message.append("Сообщение, которое мы сочли подозрительным: ```md\n").append(event.getMessage().getContentRaw()).append("\n``` \n");
+                message.append("А если ты сам являешься администратором сервера и хочешь защитить своё сообщество от фишинга, "
+                        + "то можешь добавить этого бота к себе на сервер с помощью ссылки: ").append(inviteUrl);
+
+                try {
+                    PrivateChannel channel = event.getMember().getUser().openPrivateChannel().complete();
+                    channel.sendMessage(message.toString()).complete();
+
+                    if (event.getGuild().getSelfMember().hasPermission(Permission.KICK_MEMBERS))
+                        event.getMember().kick("Рассылка фишинговых ссылок").queue();
+                } catch (Exception exception) {
+                    logger.warn("Unable to notify {} about kick", event.getMember().getUser().getAsTag(), exception);
+                }
             }
         } catch (Exception exception) {
             logger.error("Unable to process message", exception);
--- src/main/java/ru/ruscalworld/fishingguard/models/GuildSettings.java
+++ src/main/java/ru/ruscalworld/fishingguard/models/GuildSettings.java
@@ -1,13 +1,23 @@
 package ru.ruscalworld.fishingguard.models;
 
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Invite;
+import net.dv8tion.jda.api.entities.TextChannel;
+import ru.ruscalworld.fishingguard.FishingGuard;
 import ru.ruscalworld.storagelib.DefaultModel;
+import ru.ruscalworld.storagelib.Storage;
 import ru.ruscalworld.storagelib.annotations.Model;
 import ru.ruscalworld.storagelib.annotations.Property;
+import ru.ruscalworld.storagelib.exceptions.NotFoundException;
 
 import java.sql.Timestamp;
+import java.util.Optional;
 
 @Model(table = "guilds")
 public class GuildSettings extends DefaultModel {
+    private static final Storage storage = FishingGuard.getInstance().storage();
+
     @Property(column = "guild_id")
     private String guildID;
 
@@ -25,8 +35,36 @@ public class GuildSettings extends DefaultModel {
         this.guildID = guildID;
     }
 
+    public static GuildSettings getByGuild(Guild guild) {
+        try {
+            return storage.find(GuildSettings.class, "guild_id", guild.getId());
+        } catch (Exception ignored) { }
+        return new GuildSettings(guild.getId());
+    }
+
+    public Guild getGuild() {
+        return FishingGuard.getInstance().jda().getGuildById(this.getGuildID());
+    }
+
     public String getGuildID() {
         return guildID;
+    }
+
+    public Optional<Invite> getInvite() {
+        if (this.getInviteCode() == null) {
+            if (!this.getGuild().getSelfMember().hasPermission(Permission.CREATE_INSTANT_INVITE)) return Optional.empty();
+            TextChannel channel = this.getGuild().getDefaultChannel();
+            if (channel == null) channel = this.getGuild().getTextChannels().get(0);
+            if (channel == null) return Optional.empty();
+
+            try {
+                return Optional.ofNullable(channel.createInvite().setMaxAge(0).complete());
+            } catch (Exception exception) {
+                return Optional.empty();
+            }
+        }
+
+        return Optional.ofNullable(Invite.resolve(FishingGuard.getInstance().jda(), this.getInviteCode()).complete());
     }
 
     public String getInviteCode() {
